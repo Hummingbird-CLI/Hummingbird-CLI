8b8f312f239e127771dc444f40fa6d00b8330b88
Emmett Deen <emmett.deen@kickasscoding.com>
2023-09-19 15:28:33 -0400

    feat: cli project intialization

---
+++ lib/hummingbird_cli.dart
@@ -0,0 +1,10 @@
+/// hummingbird_cli, A Flutter CLI tool to kickstart new projects
+///
+/// ```sh
+/// # activate hummingbird_cli
+/// dart pub global activate hummingbird_cli
+///
+/// # see usage
+/// hummingbird --help
+/// ```
+library hummingbird_cli;
